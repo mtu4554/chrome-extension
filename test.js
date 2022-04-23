5fbfb3a43b0bae9f29b172a51d813adc22ae0a0b
Martin Belej <mbelej100@gmail.com>
2022-04-23 19:03:29 +0200

    all

--- test.js
+++ test.js
@@ -1,54 +1,10 @@
+var forbidden = ["DOCUMENT", "SCRIPT", "STYLE"]
 
-const elements = document.getElementsByTagName("p");
-
-const h3List = Array.prototype.slice.call(elements);
-
-const blured = "color: transparent; text-shadow: 0 0 8px #000;";
-
-console.log(elements);
-
-let popup;
-let unBluredElement;
-
-h3List.forEach(element => {
-
-    const style = element.style;
-    element.style = blured;
-
-    element.addEventListener("mouseenter", function(event) {
-        const rect = element.getBoundingClientRect();
-        console.log(rect.top);
-
-        
-        const html = "<h1>Word</h1>";
-        popup = document.createElement("div");
-        popup.style = "position: fixed; z-index: 100; margin-top: " + (rect.top - 100) + 
-                        "px; margin-left: " + event.pageX + "px; background-color: white; " +
-                        "border-radius: 10px; padding: 10px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;";
-        popup.innerHTML = html;
-        document.body.appendChild(popup);
-        element.style = "color: transparent; text-shadow: 0 0 8px #000;"
-        unBluredElement = element;
-        element.style = style;
-        
-    });
-
-    element.addEventListener("mouseleave", function() {
-        document.body.removeChild(popup);
-        element.style = blured;
-    });
-
-});
-
-// console.log(document);
-
-// const myList = document.body.innerText.split('\n');
-// console.log(myList);
-/*
 function iterateOverDom(element, array){
     for (let i = 0; i < element.children.length; i++) {
         text = getDirectLastText(element.children[i]).trim().replaceAll("  ", "").replaceAll("\t", "");
-        if(text.length > 0){
+        //console.log( element.tagName)
+        if(text.length > 0 &&  !forbidden.includes(element.tagName)){
             //element.children[i].id = makeid(7);
             object = {
                 "id": getSelector(element.children[i]),//element.children[i].id,
@@ -120,4 +76,87 @@ function sendForEvaluation(data, url){
 
 var wordList = iterateOverDom(document.body, []);
 console.log(wordList)
-*/
+
+
+function blurElements() {
+    const blured = "color: transparent; text-shadow: 0 0 8px #000;";
+
+    let popup;
+    let unBluredElement;
+
+    wordList.forEach(object => {
+        var element = null;
+        try {
+            element = document.querySelector(object.id);
+        } catch(e) {
+            
+        }
+
+        if(element != null) {
+            const style = element.style;
+            element.style = blured;
+
+            element.addEventListener("mouseenter", function(event) {
+                
+                element.style = "color: transparent; text-shadow: 0 0 8px #000;"
+                
+                try {
+                    const oldPopup = document.getElementById("popup");
+                    document.body.removeChild(oldPopup);
+                } catch(e) {
+                }
+
+                const rect = element.getBoundingClientRect();
+                console.log(rect.top);
+
+                
+                const html = "<h1>Word</h1>";
+                popup = document.createElement("div");
+                popup.style = "position: fixed; z-index: 100; margin-top: " + (rect.top - 100) + 
+                                "px; margin-left: " + event.pageX + "px; background-color: white; " +
+                                "border-radius: 10px; padding: 10px; box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;";
+                popup.innerHTML = html;
+                popup.id = "popup";
+                document.body.appendChild(popup);
+                unBluredElement = element;
+                element.style = style;
+            });
+
+            element.addEventListener("mouseleave", function() {
+                try {
+                    element.style = blured;
+                    const oldPopup = document.getElementById("popup");
+                    document.body.removeChild(oldPopup);
+                } catch(e) {
+                } 
+            });
+        }
+    });
+}
+
+
+let isUpdated = false;
+console.log("you hsouldnt be here");
+//on scroll -> ud = true
+
+document.addEventListener("scroll", function(e){
+    console.log("uuweee!");
+    isUpdated = false;
+})
+
+setInterval(() => {
+    if(!isUpdated){
+        console.log("resized" + isUpdated);
+        wordList = iterateOverDom(document.body, []);
+        console.log(wordList);
+        blurElements();
+    }
+    isUpdated = true;
+    console.log(isUpdated);
+}, 2000);
+
+    // console.log(document);
+
+// const myList = document.body.innerText.split('\n');
+// console.log(myList);
+
